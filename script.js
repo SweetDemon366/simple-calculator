3763dfd56cf7e2bc1ec39e8e63139dab4d2411ec
Mizodeus <151405871+SweetDemon366@users.noreply.github.com>
2026-06-02 20:56:45 +0500

    Add calculate and round result

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const subText = document.getElementById('subText')
 const buttons = document.querySelectorAll('.btn');
 
 const operators = {
-    '+': '*',
-    '-': '-',
-    '÷': '/',
-    'x': '*',
+    "+": (a, b) => a + b,
+    "−": (a, b) => a - b,
+    "×": (a, b) => a * b,
+    "÷": (a, b) => a / b
 }
 
 let isCompactDisplay = false;
+let isResultShown = false;
 
 function updateDisplay() {
     if (isCompactDisplay) {
@@ -39,8 +40,8 @@ buttons.forEach(button => {
                 break;
             
             case "÷":
-            case "x":
-            case "-":
+            case "×":
+            case "−":
             case "+":
                 addOperator(value);
                 break;
@@ -76,6 +77,7 @@ function backspace() {
 
 function addOperator(operator) {
     const lastIndex = inputArray.findLastIndex(item => Object.keys(operators).includes(item));
+    isResultShown = false;
 
     if (inputArray.length == (lastIndex + 1)) {
         inputArray.splice(-1, 1)
@@ -87,6 +89,10 @@ function addOperator(operator) {
 function addDigit(digit) {
     if (inputArray == '0') {
         inputArray.splice(0,1);
+    } else if (isResultShown) {
+        inputArray = [];
+        isResultShown = false;
+        isCompactDisplay = false;
     }
     inputArray.push(digit);
     updateDisplay()
@@ -100,14 +106,38 @@ function addDot() {
         if (number.length == 0) {
             inputArray.push('0')
         }
+        isResultShown = false;
         inputArray.push('.');
         updateDisplay();
     }
 }
 
 function calculate () {
-    mergeDigits();
+    isCompactDisplay = true;
+    isResultShown = true;
+    const arr = mergeDigits();
+    subText.textContent = inputArray.join('');
 
+    for (let i = 1; i < arr.length; i+= 2) {
+        if (arr[i] == '×' || arr[i] == '÷') {
+            let result = operators[arr[i]](
+                arr[i-1],
+                arr[i+1]
+            );
+            arr.splice(i - 1, 3, result);
+            i -= 2
+        };
+    };
+    let result = arr[0];
+
+    for (let i = 1; i < arr.length; i += 2) {
+        result = operators[arr[i]](
+            result,
+            arr[i + 1]
+        );
+    }
+    inputArray = [round(result)];
+    updateDisplay();
 }
 
 function mergeDigits() {
@@ -129,4 +159,13 @@ function mergeDigits() {
         result.push(Number(current));
     }
     return result;
+}
+
+function round (result) {
+    let str = String(result);
+
+    if (str.length <= 10) {
+        return str;
+    };
+    return Number(result).toExponential(4);
 }
